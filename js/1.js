0a2d0e4e3fd8d9a314d3921924f0fde397bf7d62
purvataranekar <purvataranekar@users.noreply.github.com>
2015-04-05 18:59:11 +0530

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -1,7 +1,7 @@
 var current_pos = {x : 0, y : 0};
 var current_vel = {x : 1, y : 0};
 var x = Math.floor((Math.random() * (10 - 0 + 1) + 0);
-//var y = Math.floor((Math.random() * (20 - 10 + 1) + 10);
+var y = Math.floor((Math.random() * (20 - 10 + 1) + 10);
 document.getElementById("random1").innerHTML= x;
 document.getElementById("random2").innerHTML= y;
 var canvas = document.getElementById("canvas");
